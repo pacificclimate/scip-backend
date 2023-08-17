a227d99a7c4acc0fc1e9799366133aa75da0ac44
Lee Zeman <lzeman@uvic.ca>
2023-08-17 12:56:53 -0700

    further refactor region queries

--- scip/api/region_helpers.py
+++ scip/api/region_helpers.py
@@ -1,38 +1,36 @@
-# The functions in this file assist the region API to build queries
-# needed to provide information about "regions" (polygons) in the postGIS
-# database. The polygons are stored in two different tables in the database,
-# Regions and ConservationUnits, but are presented identically to the user.
+"""
+The functions in this file assist the region API to build queries
+needed to provide information about "regions" (polygons) in the postGIS
+database. The polygons are stored in two different tables in the database,
+Regions and ConservationUnits, but are presented identically to the user.
 
-# Additionally, the API user may specify that they only wish to see regions
-# in which certain species of salmon are found. The two geometry-recording
-# tables have different relationships to the Population table, which records
-# salmon information. Therefore, there are four possible types of queries
-# that might need to be constructed:
-# 1. conservation unit with species: build_cu_query -> cu_with_taxon
-# 2. conservation unit without species: build_cu_query -> cu_geometry_only
-# 3. other region with species: build_region_query -> region_with_taxon
-# 4. other region without species: build_region_query -> region_geometry_only
+Additionally, the API user may specify that they only wish to see regions
+in which certain species of salmon are found. The two geometry-recording
+tables have different relationships to the Population table, which records
+salmon information. Therefore, there are four possible types of queries
+that might need to be constructed:
+ 1. conservation unit with species: build_cu_query -> cu_with_taxon
+ 2. conservation unit without species: build_cu_query -> cu_geometry_only
+ 3. other region with species: build_region_query -> region_with_taxon
+ 4. other region without species: build_region_query -> region_geometry_only
+"""
 
 from salmon_occurrence import Region, ConservationUnit, Population, Taxon
 from sqlalchemy import func
 
 
 def cu_with_taxon(session, common_name, subgroup):
-    """Returns a query that joins the conservation unit table with the population table, in order to get a list of conservation units that contain a particular salmon species"""
-    q = (
-        session.query(
-            ConservationUnit.name.label("name"),
-            ConservationUnit.code.label("code"),
-            func.ST_AsGeoJSON(ConservationUnit.boundary).label("boundary"),
-            func.ST_ASGeoJSON(ConservationUnit.outlet).label("outlet"),
-        )
-        .join(
-            Population,
-            Population.conservation_unit_id == ConservationUnit.id,
-        )
-        .join(Taxon, Population.taxon_id == Taxon.id)
-        .filter(Taxon.common_name == common_name)
+    """Returns a query that joins the conservation unit table
+    with the population table, in order to get a list of
+    conservation units that contain a particular salmon species"""
+    q = cu_geometry_only(session)
+
+    q = q.join(
+        Population,
+        Population.conservation_unit_id == ConservationUnit.id,
     )
+    q = q.join(Taxon, Population.taxon_id == Taxon.id)
+    q = q.filter(Taxon.common_name == common_name)
 
     if subgroup:
         q = q.filter(Taxon.subgroup == subgroup)
@@ -56,7 +54,8 @@ def cu_geometry_only(session):
 def build_cu_query(
     session, overlap=None, name=None, code=None, common_name=None, subgroup=None
 ):
-    """Creates an SQLalchemy query to get information about conservation units"""
+    """Creates an SQLalchemy query to get information about
+    conservation units"""
     if common_name:
         q = cu_with_taxon(session, common_name, subgroup)
     else:
@@ -75,27 +74,19 @@ def build_cu_query(
 
 
 def region_with_taxon(session, kind, common_name, subgroup):
-    """Returns a query that spatially joins the regions table with the conservation units table, in order to access salmon population info"""
-    q = (
-        session.query(
-            Region.name.label("name"),
-            Region.code.label("code"),
-            Region.kind.label("kind"),
-            func.ST_AsGeoJSON(Region.boundary).label("boundary"),
-            func.ST_AsGeoJSON(Region.outlet).label("outlet"),
-        )
-        .join(
-            ConservationUnit,
-            ConservationUnit.boundary.ST_Intersects(Region.boundary),
-        )
-        .join(
-            Population,
-            Population.conservation_unit_id == ConservationUnit.id,
-        )
-        .join(Taxon, Population.taxon_id == Taxon.id)
-        .filter(Region.kind == kind)
-        .filter(Taxon.common_name == common_name)
+    """Returns a query that spatially joins the regions table with the
+    conservation units table, in order to access salmon population info"""
+    q = region_geometry_only(session, kind)
+    q = q.join(
+        ConservationUnit,
+        ConservationUnit.boundary.ST_Intersects(Region.boundary),
     )
+    q = q.join(
+        Population,
+        Population.conservation_unit_id == ConservationUnit.id,
+    )
+    q = q.join(Taxon, Population.taxon_id == Taxon.id)
+    q = q.filter(Taxon.common_name == common_name)
 
     if subgroup:
         q = q.filter(Taxon.subgroup == subgroup)
